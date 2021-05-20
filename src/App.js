d094dc1524d8bd2d98ca6684f09962fda64608cb
katuobukova13 <65791783+katuobukova13@users.noreply.github.com>
2021-05-21 00:23:27 +0300

    1 try: signUp material-Ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+/* import { useState } from "react"; */
+import "./App.css";
+import SignUp from "./SignUp";
 
-function App() {
+const App = () => {
+  /*  const [logInData, setLogInData] = useState(initialData);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(logInData);
+  };
+ */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*    <form onSubmit={handleSubmit}>
+        <label>
+          Enter your nickname
+          <input
+            value={logInData.nickname}
+            onChange={(e) =>
+              setLogInData({ ...logInData, nickname: e.target.value })
+            }
+          />
+        </label>
+        <label>
+          Enter your email
+          <input
+            type="email"
+            value={logInData.email}
+            onChange={(e) =>
+              setLogInData({ ...logInData, email: e.target.value })
+            }
+          />
+        </label>
+        <label>
+          Enter your password
+          <input
+            type="password"
+            value={logInData.password}
+            onChange={(e) =>
+              setLogInData({ ...logInData, password: e.target.value })
+            }
+          />
+        </label>
+        <button>Submit</button>
+      </form> */}
+      <SignUp />
     </div>
   );
-}
+};
 
 export default App;
